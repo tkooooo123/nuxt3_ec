df402569568e2c257e584ea6087c671f5100c86d
tkooooo123 <tkooooo123@gmail.com>
2025-06-11 23:15:14 +0800

    新增 user model 與 seed 檔案

---
+++ server/models/User.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+}
+
+const UserSchema: Schema<IUser> = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: 'user',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export default mongoose.model("User", UserSchema);
