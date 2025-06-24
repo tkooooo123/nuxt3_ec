e6e65730f9e9bea8a1f35dcb8a882e179650601a
tkooooo123 <tkooooo123@gmail.com>
2025-06-24 23:59:55 +0800

    新增編輯分類 api

---
+++ server/api/admin/category.put.ts
@@ -0,0 +1,46 @@
+import Category from "@/server/models/Category";
+import { H3Event } from "h3";
+
+export default defineEventHandler(async (event: H3Event) => {
+  try {
+    const body = await readBody(event);
+    const { id, name, description } = body;
+
+    if (!id) {
+      return createError({ statusCode: 400, statusMessage: '請提供分類 ID' });
+    }
+
+    if (!name) {
+      return createError({ statusCode: 400, statusMessage: '請輸入分類名稱' });
+    }
+
+    if (!description) {
+      return createError({ statusCode: 400, statusMessage: '請輸入分類描述' });
+    }
+
+    const category = await Category.findByIdAndUpdate(
+      id,
+      { name, description },
+      { new: true } //回傳更新後資料
+    );
+
+    if (!category) {
+      return createError({ statusCode: 404, statusMessage: '找不到分類' });
+    }
+
+     return event.respondWith(
+      new Response(
+        JSON.stringify({
+          message: "更新成功!",
+          
+        }),
+        { status: 200, headers: { "Content-Type": "application/json" } }
+      )
+    );
+  } catch (error: any) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: error.message || "伺服器錯誤",
+    });
+  }
+});
