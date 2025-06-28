59522d04cffd78598397d4252c6fd03be98f5638
tkooooo123 <tkooooo123@gmail.com>
2025-06-28 18:15:46 +0800

    建立新增文章 api

---
+++ server/api/admin/article.post.ts
@@ -0,0 +1,40 @@
+import Article from "~/server/models/Article";
+import { H3Event } from "h3";
+
+export default defineEventHandler(async (event: H3Event) => {
+try {
+    const body = await readBody(event);
+    console.log(body)
+    const { title, author, tags, content, is_public, date } = body
+    if (!title) {
+      return createError({ statusCode: 400, statusMessage: '請輸入文章標題' });
+    }
+     if (!author) {
+      return createError({ statusCode: 400, statusMessage: '請輸入文章作者' });
+    }
+     if (tags.length === 0) {
+      return createError({ statusCode: 400, statusMessage: '請輸入文章標籤' });
+    }
+     if (!content) {
+      return createError({ statusCode: 400, statusMessage: '請輸入文章標籤' });
+    }
+     if (!is_public) {
+      return createError({ statusCode: 400, statusMessage: '請選擇是否公開' });
+    }
+       if (!date) {
+      return createError({ statusCode: 400, statusMessage: '請選擇公告日期' });
+    }
+
+     const article = await Article.create(body);
+        if (article) {
+          return {
+               message: "新增成功!",
+          }
+        }
+} catch (error: any) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: error.message || "伺服器錯誤",
+    });
+  }
+})
---
+++ server/models/Article.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IArticle extends Document {
+  title: string
+  author: string
+  is_public: boolean
+  tags: string[]
+  content: string
+  date: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+// 建立 Article 的 Schema
+const ArticleSchema: Schema<IArticle> = new Schema<IArticle>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    is_public: { type: Boolean, default: false },
+    tags: { type: [String], default: [] },
+    content: { type: String, required: true },
+    date: {  type: String, required: true }
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: true } // 自動維護 createdAt 和 updatedAt
+  }
+);
+
+export default mongoose.model<IArticle>("Article", ArticleSchema);
