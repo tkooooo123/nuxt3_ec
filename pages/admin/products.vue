ff15bdd92d9f18e3373611092a71c35ad7ca7e17
skytek-sy <tkooooo123@gmail.com>
2025-07-23 10:26:37 +0800

    修正表格欄位

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -452,7 +452,12 @@ const deleteProduct = async () => {
           </button>
         </div>
         <el-table :data="productList" class="mt-6">
-          <el-table-column label="圖片" width="105">
+          <el-table-column label="No" width="60">
+            <template #default="scope">
+              {{ scope.$index + 1 }}
+            </template>
+          </el-table-column>
+          <el-table-column label="主圖" width="105">
             <template #default="scope">
               <img
                 :src="scope.row.image"
@@ -461,7 +466,61 @@ const deleteProduct = async () => {
               />
             </template>
           </el-table-column>
-          <el-table-column label="名稱" prop="name"></el-table-column>
+          <el-table-column label="名稱" prop="name" width="170">
+            <template #default="scope">
+              <div class="flex flex-col">
+                <span>{{ scope.row.name }}</span>
+                <div class="flex gap-2 mt-2">
+                  <span
+                    v-if="scope.row.is_newest"
+                    class="bg-#F7eee9 flex text-primary py-1.5 px-2 rounded-7.5"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                      />
+                    </svg>
+                    新品
+                  </span>
+                  <span
+                    v-if="scope.row.is_hottest"
+                    class="bg-primary flex text-#F7eee9 py-1.5 px-2 rounded-7.5"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z"
+                      />
+                    </svg>
+
+                    TOP
+                  </span>
+                </div>
+              </div>
+            </template>
+          </el-table-column>
           <el-table-column
             label="原價"
             prop="origin_price"
@@ -477,19 +536,13 @@ const deleteProduct = async () => {
             prop="quantity"
             width="60"
           ></el-table-column>
-          <el-table-column label="狀態">
+          <el-table-column label="是否啟用">
             <template #default="scope">
               <div class="flex flex-col gap-1">
-                <el-tag v-if="scope.row.is_hottest" type="danger" size="small"
-                  >最熱門</el-tag
+                <el-tag v-if="scope.row.isEnabled" type="success"
+                  >已啟用</el-tag
                 >
-                <el-tag v-if="scope.row.is_newest" type="success" size="small"
-                  >最新</el-tag
-                >
-                <el-tag v-if="scope.row.isEnabled" type="primary" size="small"
-                  >啟用</el-tag
-                >
-                <el-tag v-else type="info" size="small">停用</el-tag>
+                <el-tag v-else type="danger">未啟用</el-tag>
               </div>
             </template>
           </el-table-column>
