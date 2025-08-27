3b512d56101641b02a5a401c59ce24d60f972280
skytek-sy <tkooooo123@gmail.com>
2025-08-27 09:15:31 +0800

    新增手機板樣式

--- pages/admin/order/index.vue
+++ pages/admin/order/index.vue
@@ -15,6 +15,14 @@ const ordersList = ref<OrderResponse[]>([])
 const deleteDialogVisible = ref<boolean>(false)
 const selectedOrderId = ref<string>('')
 
+const statusMap: Record<string, string> = {
+  paid: '已付款',
+  shipping: '配送中',
+  shipped: '已送達',
+  completed: '已完成',
+  cancelled: '已取消'
+}
+
 // 分頁相關
 const {
   currentPage,
@@ -77,7 +85,7 @@ onMounted(() => {
     <div class="w-full">
       <div class="p-6">
         <h1 class="text-8">訂單管理</h1>
-        <el-table :data="pagedOrders" class="mt-6">
+        <el-table :data="pagedOrders" class="mt-6 hidden md:block">
           <el-table-column label="No" width="60">
             <template #default="scope">
               <span>{{ scope.$index + 1 }}</span>
@@ -119,6 +127,7 @@ onMounted(() => {
 
           <el-table-column label="狀態">
             <template #default="scope">
+
               <span
                 v-if="
                   scope.row.status === 'pending' &&
@@ -126,12 +135,8 @@ onMounted(() => {
                 "
                 >待付款</span
               >
-              <span v-else-if="scope.row.status === 'paid'">已付款</span>
-              <span v-else-if="scope.row.status === 'shipping'">配送中</span>
-              <span v-else-if="scope.row.status === 'shipped'">已送達</span>
-              <span v-else-if="scope.row.status === 'completed'">已送達</span>
-              <span v-else-if="scope.row.status === 'cancelled'">已取消</span>
-              <span v-else>處理中</span>
+              <span v-else>{{ statusMap[scope.row.status] || '處理中' }}</span>
+             
             </template>
           </el-table-column>
           <el-table-column label="功能" width="160">
@@ -158,6 +163,50 @@ onMounted(() => {
             </template>
           </el-table-column>
         </el-table>
+
+        <!-- Mobile -->
+         <div class="md:hidden mt-4">
+          <div v-for="(order, i) in pagedOrders" :key="order.id"
+          class="flex flex-col p-6 mb-4 border border-#E7e7e7 border-solid rounded-4"
+          >
+          <span>No： {{ (currentPage - 1) * pageSize + i + 1 }}</span>
+          <span class="mt-2">訂單編號：{{ order.id }}</span>
+          <span class="mt-2">訂購人： {{ order.shipping.name }}</span>
+          <span class="mt-2">Email： {{ order.shipping.email }}</span>
+          <div class="flex mt-2">
+            <span>訂購產品/<small>數量</small>：</span>
+            <div class="flex flex-col">
+              <span v-for="product in order.items" :key="product.name">
+                {{ product.name }} x {{ product.quantity }}
+              </span>
+            </div>
+          </div>
+          <span class="mt-2">總金額： ${{ order.total }}</span>
+          <span class="mt-2">狀態： 
+            <span v-if="order.status === 'pending' && order.payment ==='credit_card'">待付款</span>
+            <span v-else>{{ statusMap[order.status] || `處理中` }}</span>
+          </span>
+          <div class="flex mt-2">
+                <button
+                  @click="navigateTo(`/admin/order/${order.id}`)"
+                  class="hover:bg-blue-light bg-white text-blue-light border border-blue-light border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200 mr-2"
+                >
+                  編輯
+                </button>
+                <button
+                  class="hover:bg-alert bg-white text-alert border border-alert border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200"
+                  @click="
+                    () => {
+                      deleteDialogVisible = true
+                      selectedOrderId = order.id
+                    }
+                  "
+                >
+                  刪除
+                </button>
+              </div>
+        </div>
+         </div>
 
         <!-- 分頁 -->
         <div class="flex justify-center mt-6">
