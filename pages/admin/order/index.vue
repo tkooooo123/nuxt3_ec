eb323ec94183632d43796e781e68b22e5b75c109
tkooooo123 <tkooooo123@gmail.com>
2025-11-20 18:25:24 +0800

    fix: 修正為使用 useAsyncData 函數請求訂單資料

--- pages/admin/order/index.vue
+++ pages/admin/order/index.vue
@@ -11,7 +11,7 @@ definePageMeta({
 })
 const token = useCookie('token')
 const loadingStore = useLoadingStore()
-const ordersList = ref<OrderResponse[]>([])
+
 const deleteDialogVisible = ref<boolean>(false)
 const selectedOrderId = ref<string>('')
 
@@ -23,16 +23,13 @@ const statusMap: Record<string, string> = {
   cancelled: '已取消'
 }
 
-// 分頁相關
-const {
-  currentPage,
-  pageSize,
-  pagedData: pagedOrders
-} = usePagination<OrderResponse>(ordersList, 10)
 
-const getOrders = async () => {
-  loadingStore.show()
+const {
+  data: ordersList,
+  refresh: refreshOrdersList
+} = useAsyncData('ordersList', async () => {
   try {
+    loadingStore.show()
     const res = await $fetch<ApiResponse<OrderResponse[]>>(
       '/api/admin/orders',
       {
@@ -41,17 +38,26 @@ const getOrders = async () => {
         }
       }
     )
-    if (res.data) {
-      ordersList.value = res.data
-    }
+    return res.data ?? []
   } catch (error: unknown) {
     if (error instanceof FetchError) {
       toast.error(`${error.data?.statusMessage}`)
     }
+    return []
   } finally {
     loadingStore.hide()
   }
-}
+}, {
+  default: () => []
+})
+
+// 分頁相關
+const {
+  currentPage,
+  pageSize,
+  pagedData: pagedOrders
+} = usePagination<OrderResponse>(ordersList, 10)
+
 const handleDelete = async () => {
   loadingStore.show()
   try {
@@ -64,7 +70,7 @@ const handleDelete = async () => {
 
     toast.success(`${res.message}`)
 
-    await getOrders()
+    await refreshOrdersList()
     deleteDialogVisible.value = false
   } catch (error: unknown) {
     if (error instanceof FetchError) {
@@ -75,9 +81,6 @@ const handleDelete = async () => {
   }
 }
 
-onMounted(() => {
-  getOrders()
-})
 </script>
 
 <template>
@@ -114,11 +117,7 @@ onMounted(() => {
               <div class="flex"><span>訂購產品/</span><small>數量</small></div>
             </template>
             <template #default="scope">
-              <div
-                v-for="item in scope.row.items"
-                :key="item.id"
-                class="flex flex-col"
-              >
+              <div v-for="item in scope.row.items" :key="item.id" class="flex flex-col">
                 <span class="font-600">{{ item.name }}</span>
                 <span> x {{ item.quantity }}</span>
               </div>
@@ -133,24 +132,19 @@ onMounted(() => {
           <el-table-column label="狀態">
             <template #default="scope">
 
-              <span
-                v-if="
-                  scope.row.status === 'pending' &&
-                  scope.row.payment === 'credit_card'
-                "
-                >待付款</span
-              >
+              <span v-if="
+                scope.row.status === 'pending' &&
+                scope.row.payment === 'credit_card'
+              ">待付款</span>
               <span v-else>{{ statusMap[scope.row.status] || '處理中' }}</span>
-             
+
             </template>
           </el-table-column>
           <el-table-column label="功能" width="160">
             <template #default="scope">
               <div class="flex">
-                <button
-                  @click="navigateTo(`/admin/order/${scope.row.id}`)"
-                  class="hover:bg-blue-light bg-white text-blue-light border border-blue-light border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200 mr-2"
-                >
+                <button @click="navigateTo(`/admin/order/${scope.row.id}`)"
+                  class="hover:bg-blue-light bg-white text-blue-light border border-blue-light border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200 mr-2">
                   編輯
                 </button>
                 <button
@@ -160,8 +154,7 @@ onMounted(() => {
                       deleteDialogVisible = true
                       selectedOrderId = scope.row.id
                     }
-                  "
-                >
+                  ">
                   刪除
                 </button>
               </div>
@@ -170,73 +163,62 @@ onMounted(() => {
         </el-table>
 
         <!-- Mobile -->
-         <div class="md:hidden mt-4">
+        <div class="md:hidden mt-4">
           <div v-for="(order, i) in pagedOrders" :key="order.id"
-          class="flex flex-col p-6 mb-4 border border-#E7e7e7 border-solid rounded-4"
-          >
-          <span>No： {{ (currentPage - 1) * pageSize + i + 1 }}</span>
-          <span class="mt-2">訂單編號：{{ order.id }}</span>
-          <span class="mt-2">訂購日期：{{ order.createdAt.slice(0, 10) }}</span>
-          <span class="mt-2">訂購人： {{ order.shipping.name }}</span>
-          <span class="mt-2">Email： {{ order.shipping.email }}</span>
-          <div class="flex mt-2">
-            <span>訂購產品/<small>數量</small>：</span>
-            <div class="flex flex-col">
-              <span v-for="product in order.items" :key="product.name">
-                {{ product.name }} x {{ product.quantity }}
-              </span>
+            class="flex flex-col p-6 mb-4 border border-#E7e7e7 border-solid rounded-4">
+            <span>No： {{ (currentPage - 1) * pageSize + i + 1 }}</span>
+            <span class="mt-2">訂單編號：{{ order.id }}</span>
+            <span class="mt-2">訂購日期：{{ order.createdAt.slice(0, 10) }}</span>
+            <span class="mt-2">訂購人： {{ order.shipping.name }}</span>
+            <span class="mt-2">Email： {{ order.shipping.email }}</span>
+            <div class="flex mt-2">
+              <span>訂購產品/<small>數量</small>：</span>
+              <div class="flex flex-col">
+                <span v-for="product in order.items" :key="product.name">
+                  {{ product.name }} x {{ product.quantity }}
+                </span>
+              </div>
+            </div>
+            <span class="mt-2">總金額： ${{ order.total }}</span>
+            <span class="mt-2">狀態：
+              <span v-if="order.status === 'pending' && order.payment === 'credit_card'">待付款</span>
+              <span v-else>{{ statusMap[order.status] || `處理中` }}</span>
+            </span>
+            <div class="flex mt-2">
+              <button @click="navigateTo(`/admin/order/${order.id}`)"
+                class="hover:bg-blue-light bg-white text-blue-light border border-blue-light border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200 mr-2">
+                編輯
+              </button>
+              <button
+                class="hover:bg-alert bg-white text-alert border border-alert border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200"
+                @click="
+                  () => {
+                    deleteDialogVisible = true
+                    selectedOrderId = order.id
+                  }
+                ">
+                刪除
+              </button>
             </div>
           </div>
-          <span class="mt-2">總金額： ${{ order.total }}</span>
-          <span class="mt-2">狀態： 
-            <span v-if="order.status === 'pending' && order.payment ==='credit_card'">待付款</span>
-            <span v-else>{{ statusMap[order.status] || `處理中` }}</span>
-          </span>
-          <div class="flex mt-2">
-                <button
-                  @click="navigateTo(`/admin/order/${order.id}`)"
-                  class="hover:bg-blue-light bg-white text-blue-light border border-blue-light border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200 mr-2"
-                >
-                  編輯
-                </button>
-                <button
-                  class="hover:bg-alert bg-white text-alert border border-alert border-solid hover:text-white rounded-2 w-16 h-10 cursor-pointer transition-all duration-200"
-                  @click="
-                    () => {
-                      deleteDialogVisible = true
-                      selectedOrderId = order.id
-                    }
-                  "
-                >
-                  刪除
-                </button>
-              </div>
         </div>
-         </div>
 
         <!-- 分頁 -->
         <div class="flex justify-center mt-6">
-          <Pagination
-            :total="ordersList.length"
-            :page-size="pageSize"
-            v-model:currentPage="currentPage"
-          />
+          <Pagination  :total="ordersList?.length" :page-size="pageSize" v-model:currentPage="currentPage" />
         </div>
       </div>
       <el-dialog title="刪除訂單" v-model="deleteDialogVisible">
         <div>
           <p>訂單「{{ selectedOrderId }}」刪除後將無法復原，你確定要刪除嗎？</p>
           <div class="flex justify-end">
-            <button
-              class="rounded-4 cursor-pointer px-4 py-2 mr-3 hover:brightness-90 transition duration-200"
-              @click="deleteDialogVisible = false"
-            >
+            <button class="rounded-4 cursor-pointer px-4 py-2 mr-3 hover:brightness-90 transition duration-200"
+              @click="deleteDialogVisible = false">
               取消
             </button>
             <button
               class="bg-red text-white rounded-4 hover:brightness-90 h-10 px-4 cursor-pointer transition duration-200"
-              @click="handleDelete"
-            >
+              @click="handleDelete">
               確定
             </button>
           </div>
